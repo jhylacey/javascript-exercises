76899b6945e3254498f8e884dbd60b4acf55b756
jhylacey <jhylacey@proton.me>
2023-03-09 15:12:42 -0600

    Finish Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    let newString = "";
+    if (num === NaN || num === undefined || num === null || num < 0) {
+        return "ERROR"
+    } else {
+        for (let i = 1; i <= num; i++) {
+            newString += string
+        };
+    }
+    return newString;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
