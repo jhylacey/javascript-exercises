ab0db725dddc45b2f2dc481d0dcf4735a2f07e79
jhylacey <jhylacey@proton.me>
2023-03-10 11:18:48 -0600

    Finish Exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,6 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = far => +((far - 32) / 1.8000).toFixed(1);
 
-const convertToFahrenheit = function() {
-};
-
+const convertToFahrenheit = cels => +((cels * 1.8000) + 32.00).toFixed(1);
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
