be71dc41f9a6f676f53f7ec8cfea0b931515df24
jhylacey <jhylacey@proton.me>
2023-03-13 10:34:00 -0500

    Finished Exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
-};
-
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const add = function (x, y) {
+  return x + y
 
 };
 
-const power = function() {
-	
+const subtract = function (x, y) {
+  return x - y
+
 };
 
-const factorial = function() {
-	
+const sum = function (array) {
+  let total = 0;
+  for (let i = 0; i < array.length; i++) {
+    total += array[i];
+  }
+  return total;
+};
+
+const multiply = function (array) {
+  let total = 1;
+  for (let i = 0; i < array.length; i++) {
+    total *= array[i];
+  }
+  return total;
+};
+
+
+
+const power = function (x, y) {
+  return x ** y
+};
+
+const factorial = function (num) {
+  if (num === 0 || num === 1)
+    return 1;
+  for (var i = num - 1; i >= 1; i--) {
+    num *= i;
+  }
+  return num;
 };
 
 // Do not edit below this line
